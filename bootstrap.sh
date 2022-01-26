86da3c60d1129318927f0987ab507b04bff194b6
Silviu Oprea <silviu.oprea@ed.ac.uk>
2022-01-26 12:48:16 +0000

    a

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,13 @@
+conda install nodejs
+
 pip install numpy scipy tqdm torch
 pip install "jupyterlab>=3" "ipywidgets>=7.6"
 pip install jupyter-dash
-pip install ipympl
+
+pip install --upgrade jupyterlab jupyterlab-git jupyterlab-lsp
+pip install 'python-lsp-server[all]'
+pip install lckr-jupyterlab-variableinspector
+pip install jupyterlab-drawio
+pip install aquirdturtle_collapsible_headings
+jupyter labextension install jupyterlab-spreadsheet
+
